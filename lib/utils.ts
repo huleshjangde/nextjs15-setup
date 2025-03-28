3ab31317ea67f8861ad41a9f91a0241aa03d3437
huleshjangde <huleshjangde1@gmail.com>
2025-03-28 09:39:30 +0530

    zustand added

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+export function objectToQueryString(object: any) {
+  const keys = Object.keys(object);
+  const keyValuePairs = keys.map((key) => {
+    return encodeURIComponent(key) + '=' + encodeURIComponent(object[key]);
+  });
+  return keyValuePairs.join('&');
+}
